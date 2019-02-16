fa5dd2a4d4e617360f9baeb6ee0b4cb4acbb0fb1
hrzp <hamidreza.num1@gmail.com>
2019-02-16 18:14:48 +0330

    first commit

---
+++ asset/js/main.js
@@ -0,0 +1,220 @@
+var globalTimer = null;
+var pointerPause = false;
+var pointerState = 0;
+
+var myFullpage = new fullpage('#main', {
+    navigation: true,
+    scrollingSpeed: 1000,
+    slidesNavigation: true,
+    scrollHorizontally: true,
+    licenseKey: 'C74E309E-891D44FF-980DCF32-4E4E704B',
+    menu: '#myMenu',
+    anchors: ['home', 'blankDAO', 'possibilities', 'buy', 'rewards', 'roadmap', 'f'],
+});
+
+$(document).ready(function () {
+    collapseWorker();
+    setTimeout(function () {
+        $('body').css("visibility", "visible");
+    }, 500);
+    updateBDTamount();
+});
+
+function clearListStyle() {
+    for (let i in pointerList) {
+        for (let j in pointerList[i].items) {
+            let item = pointerList[i].items[j];
+            $('#' + item.id).parent().css("list-style-image", "none");
+            $('#' + item.id).parent().css("list-style-type", "none");
+        }
+    }
+}
+
+function changePointer(id) {
+    for (let i in pointerList) {
+        for (let j in pointerList[i].items) {
+            let item = pointerList[i].items[j];
+            if (item.id == id) {
+                $('#' + item.id).parent().css("list-style-image", "url('asset/image/r1.png')");
+                $('#text-content').text(item.text);
+            }
+        }
+    }
+}
+
+function pointerInterval() {
+    let pointer = innerPointer = null;
+    let i = j = 0;
+    globalTimer = setInterval(function () {
+        let url = window.location.hash;
+        if (url != "#possibilities") {
+            return;
+        }
+        if (pointerPause) {
+            return;
+        }
+        i = pointer ? 0 : i;
+        j = innerPointer ? 0 : j;
+        if (j >= pointerList[i].items.length) {
+            j = 0;
+            $('.new-li-' + i).find('ul').slideUp();
+            i++;
+            pointerState = i;
+            if (i >= pointerList.length) {
+                i = 0;
+            }
+            $('.new-li-' + i).find('ul').slideDown();
+        }
+        clearListStyle();
+        let item = pointerList[i].items[j];
+        $('#' + item.id).parent().css("list-style-image", "url(asset/image/r1.png)");
+        $('#text-content').text(item.text);
+        j++;
+    }, 10000);
+}
+
+function collapseWorker() {
+    possElCreator();
+    $(document).on("click", "a.chapter", function () {
+        clearInterval(globalTimer);
+        clearListStyle();
+        changePointer($(this).attr('id'));
+    });
+    $('.coll-child').hide();
+    $('.coll-parent').click(function () {
+      if ($(this).hasClass('new-li-'+pointerState)) {
+        return;
+      }
+      $('.new-li-' + pointerState).find('ul').slideUp();
+      classes = $(this).attr('class').split(/\s+/);
+      $.each(classes, function( index, value ) {
+        if( value.includes('new-li-') ) {
+          pointerState = parseInt(value.split("-").pop());
+          $('.new-li-' + pointerState).find('ul').slideDown();
+        }
+      });
+    });
+
+    $('.new-li-0').find('ul').slideDown();
+    $(".chapter")
+        .mouseover(function () {
+            pointerPause = true;
+        })
+        .mouseout(function () {
+            pointerPause = false;
+        });
+}
+
+$('.blog-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    navText: [
+        "<i class='fa fa-angle-left'></i>",
+        "<i class='fa fa-angle-right'></i>"
+    ],
+    autoplay: true,
+    mergeFit: true,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 3
+        }
+    }
+});
+$('.team-carousel').owlCarousel({
+    margin: 10,
+    navText: [
+        "<i class='fa fa-angle-left'></i>",
+        "<i class='fa fa-angle-right'></i>"
+    ],
+    autoplay: true,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 4
+        }
+    }
+});
+
+function possElCreator() {
+    for (var i in pointerList) {
+        $(".list-title").append($("<li class='new-li-" + i + " coll-parent'><p class=coll-title>" + pointerList[i].header + "</p></li>"));
+        $(".new-li-" + i).append($("<ul class='new-ul-" + i + " coll-child'></ul>"));
+        for (var j in pointerList[i].items) {
+            let item = pointerList[i].items[j];
+            $('.new-ul-' + i).append($("<li class='p-chapter'><a href='#' id='" + item.id + "' class='chapter'>" + item.title + "</a></li>"));
+        }
+    }
+    pointerInterval();
+}
+
+$("#js-rotating").Morphext({
+    animation: "bounceIn",
+    separator: ",",
+    speed: 2000,
+    complete: function () {}
+});
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        if (slides[i].style.display == "block") {
+            animateCss(slides[i], 'fadeOutLeftBig', true)
+        } else {
+            slides[i].style.display = "none";
+        }
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" m-active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    animateCss(slides[slideIndex - 1], 'fadeInRightBig', false)
+    dots[slideIndex - 1].className += " m-active";
+}
+
+function hideElement(element) {
+    element.style.display = "none";
+}
+
+function animateCss(node, animationName, hide) {
+    node.classList.add('animated', animationName)
+
+    function handleAnimationEnd() {
+        node.classList.remove('animated', animationName)
+        node.removeEventListener('animationend', handleAnimationEnd)
+        if (hide) hideElement(node);
+    }
+    node.addEventListener('animationend', handleAnimationEnd)
+}
---
+++ asset/js/pointerList.js
@@ -0,0 +1,67 @@
+var pointerList = [
+  {
+    header: 'First Decentralized Ledger Based On Proof Of Uniqueness',
+    items: [
+      {
+        id: 'hs0',
+        title: 'High Security',
+        text: "Decentralized ledgers like blockchain are vulnerable to double-spending attacks from owners of strong processing power or large numbers of stakes.BlankDAO, for the first time, can have a decentralized ledger that is immune to double spend attacks by giving every individual a single vote regardless of their processing power or number of stakes.",
+      },
+      {
+        id: 'zf1',
+        title: 'Zero Fees',
+        text: "BlankDAO prevents sybil attacks by relying on proof of uniqueness (not POW or POS) and because the costs of decentralized hosting are paid by the DAO as a part of the periodic distribution of generated tokens, DApps can require zero fees from end users for reasonable amounts of usage.",
+      },
+      {
+        id: 'ds2',
+        title: 'Decentralized Scalability',
+        text:  "In previous approaches, there was a see-saw relationship between decentralization and scalability. In other words, higher decentralization reduces scalability and vice versa. In BlankDAO however, proof of uniqueness results in high scalability by maintaining governance of all users while hosting the ledger on a reasonable number of hosts. Simply put, blockchain DApps are hosted by all but governed by some. In contrast, BlankDApps are actually hosted by some but governed by all.",
+      },
+    ]
+  },
+  {
+    header: 'Brand New Distribution Of Tokens',
+    items: [
+      {
+        id: 'uv01',
+        title: 'Unchained Value',
+        text: "BlankDAO’s distribution of tokens results in faster advancement of the ecosystem. In blockchain, newly generated tokens are distributed among miners to provide security. A process that ends in low scalability for developers and high costs for end users as well. BlankDAO ensures security with minimum costs by using POU to prevent sybil attacks. Therefore, for the first time, the created value can be used for creating added value.",
+      },
+      {
+        id: 'iosd02',
+        title: 'Incentivized Open Source Development',
+        text: "In BlankDAO a part of newly generated tokens is distributed among developers to encourage innovations and advancements.",
+      },
+      {
+        id: 'im03',
+        title: 'Incentivized MarketPlace',
+        text: "A part of generated tokens in BlankDAO is used as incentive for Providers of goods and services like supermarkets and taxi-drivers to support and accept native tokens in exchange for goods and services they offer. This approach expands the utility of tokens beyond digital services.",
+      },
+      {
+        id: 'ieu04',
+        title: 'Incentivized End Users',
+        text: "Blockchain DApps tend to suffer from low adoption because most users are still more interested in their centralized counterparts. Network effect also prevents their high adoption in early stages. Accordingly, in BlankDAO, early users are incentivized by a part of newly generated tokens not just because of accepting early adoption difficulties but also for putting out their social capital to attract larger adoption.",
+      },
+    ]
+  },
+  {
+    header: 'New Financial Possibilities',
+    items: [
+      {
+        id: 'de01',
+        title: 'Decentralized Exchange',
+        text: 'Current decentralized exchanges not only suffer from  low scalability but, more importantly, they can only support tokens that are native to one blockchain. DEX on Ethereum for instance can only support ETH and ERC-20 tokens. There is no way for them to support Bitcoin, ZCash, etc. without relying on a third party. This is the first time we can implement a decentralized exchange based on 1 vote per person. An exchange that is as scalable as their central counterparts. They are fully open source. People funds are saved in multi-signature cold wallets, keys of which are in control of a reasonable number of delegates selected by people.',
+      },
+      {
+        id: 'dcn02',
+        title: 'Decentralized Credit Network',
+        text: "Credit assessment and credit card issuance in so far has always exclusively been done by centralized organizations. Blockchain could never solve this problem since it lacked a decentralized ID system in which users can not escape their past behavior and negative reputation. BrightID, for the first time, enables BlankDAO to have credit assessment DApps that rely on end users’ social credit to provide end users with loans accordingly. ",
+      },
+      {
+        id: 'rb03',
+        title: 'Reserve Banking',
+        text: "In current monetary systems, reserve banks issue money and back it by their gold reserves.Similarly, BlankDAO issues stablecoins and back them by its native tokens. The stablecoins are neither backed by nor pegged to fiat money. This is the very first time that a new generation of stable money is issued that unlike government issued money, has zero inflation forever and is pegged to a CPI defined by people and not local governments.",
+      },
+    ]
+  },
+];
